deb5384c1c10499b1ff86390a89ef8cad4cbb387
TarCarbon <metalcarbon58@gmail.com>
2023-11-13 12:08:02 +0100

    commit before first start programm

--- src/main/java/org/example/db/FlywayMigration.java
+++ src/main/java/org/example/db/FlywayMigration.java
@@ -1,11 +1,18 @@
 package org.example.db;
 
+import org.apache.log4j.Logger;
 import org.flywaydb.core.Flyway;
 
 public class FlywayMigration {
 
+    final private static Logger logger = Logger.getLogger(FlywayMigration.class);
     public static void migration(){
-        Flyway.configure().dataSource("jdbc:h2:~/db/db", "user", "password")
-                .load().migrate();
+        try {
+            Flyway.configure().dataSource("jdbc:h2:~/db/db", "user", "password")
+                    .load().migrate();
+            logger.info("migration is successful");
+        } catch (Exception e) {
+            logger.error("Migration fatal: " + e);
+        }
     }
 }
