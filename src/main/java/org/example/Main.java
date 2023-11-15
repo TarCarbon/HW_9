30aa90a59bd96a11be4b54c8e2660f644c14bc8e
TarCarbon <metalcarbon58@gmail.com>
2023-11-15 08:40:50 +0100

    finish

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,28 @@ package org.example;
 
 import org.apache.log4j.Logger;
 import org.example.db.FlywayMigration;
+import org.example.entity.Resident;
 import org.example.entity.ResidentsInfo;
 import org.example.service.FindResident;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.util.List;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     final private static Logger logger = Logger.getLogger(Main.class);
-    public static void main(String[] args) {
+    public static <residentsInfoList> void main(String[] args) {
         logger.info("Program is started");
         FlywayMigration.migration();
-        List<ResidentsInfo> residentsInfoList = FindResident.findResidentWithCriteriaAPI();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnitOSBB");
+        EntityManager em = emf.createEntityManager();
+        List<ResidentsInfo> residentsInfoList = FindResident.findResidentWithCriteriaAPI(em);
+        emf.close();
+        for ( ResidentsInfo list: residentsInfoList) {
+            System.out.println(residentsInfoList);
+        }
     }
 }
--- src/main/java/org/example/service/FindResident.java
+++ src/main/java/org/example/service/FindResident.java
@@ -5,18 +5,18 @@ import org.example.entity.*;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FindResident {
-    public static List<ResidentsInfo> findResidentWithCriteriaAPI(){
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnitOSBB");
-        EntityManager em = emf.createEntityManager();
-        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
-        CriteriaQuery<ResidentsInfo> residentsInfoCQ = criteriaBuilder.createQuery(ResidentsInfo.class);
+    public static List<ResidentsInfo> findResidentWithCriteriaAPI(EntityManager em){
 
-        Root<Resident> residentRoot = residentsInfoCQ.from(Resident.class);
+        CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+        CriteriaQuery<ResidentsInfo> criteriaQuery = criteriaBuilder.createQuery(ResidentsInfo.class);
+
+        Root<Resident> residentRoot = criteriaQuery.from(Resident.class);
         Join<Resident, Apartment> apartmentJoin = residentRoot.join("apartments");
         Join<Apartment, Building> buildingJoin = apartmentJoin.join("buildings");
         Join<Resident, PropertyRights> propertyRightsJoin = residentRoot.join("propertyRights");
@@ -24,12 +24,18 @@ public class FindResident {
         Predicate driveIntoTheTerritoryPredicate = criteriaBuilder.equal(residentRoot.get("driveIntoTheTerritory"), 0);
         Predicate checkCountLess = criteriaBuilder.lessThan(residentRoot.get("property_right_id"), 3);
 
+        criteriaQuery.multiselect(
+                residentRoot.get("id"),
+                residentRoot.get("name"),
+                residentRoot.get("surname"),
+                residentRoot.get("email"),
+                apartmentJoin.get("number"),
+                apartmentJoin.get("area"),
+                buildingJoin.get("address")).where(driveIntoTheTerritoryPredicate, checkCountLess);
 
-
-        List<ResidentsInfo> residents = em.createQuery(residentsInfoCQ).getResultList();
-
+        TypedQuery<ResidentsInfo> typedQuery = em.createQuery(criteriaQuery);
+        List<ResidentsInfo> residents = typedQuery.getResultList();
         em.close();
-        emf.close();
         return residents;
     }
 }
